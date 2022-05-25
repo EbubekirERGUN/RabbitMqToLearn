77c6103d47bd8c1ade244b0fe36e03b01a1f3cff
Ebubekir ERGÜN <ebubekir.ergun@procenne.com>
2022-05-25 10:48:18 +0300

    Revert "Revert "Revert "Revert "A simple and fanout project example was created using Rabbitmq.""""
    This reverts commit 5415362e26941577cde899d9f43f987d82451c66.

---
+++ RabbitMq.Fanout.Subscriber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+var randomQueueName = channel.QueueDeclare().QueueName;
+
+
+
+channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(randomQueueName, false, consumer);
+
+Console.WriteLine("Waiting for messages...");
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine(" [x] Received {0}", message);
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+
+Console.ReadLine();
---
+++ RabbitMq.Subscribe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+// channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("hello", false, consumer);
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine(" [x] Received {0}", message);
+};
+
+
+Console.ReadLine();
