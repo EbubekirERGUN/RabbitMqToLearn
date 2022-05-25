6dde95218e9f61127c62c965718482e90553eebb
Ebubekir ERGÜN <ebubekir.ergun@procenne.com>
2022-05-25 10:44:31 +0300

    Revert "Revert "A simple and fanout project example was created using Rabbitmq.""
    This reverts commit 63bd5ee399bd55aa292c9e32eea05b96eee80424.

---
+++ RabbitMq.Fanout/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+Enumerable.Range(1, 50).ToList().ForEach(i =>
+{
+    var message = $"Log {i}";
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "logs-fanout",
+                         routingKey: "",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($" [x] Sent {message}");
+});
