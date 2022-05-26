1df9600984a6f9386571a416ce800f18c8686510
Ebubekir ERGÜN <ebubekir.ergun@procenne.com>
2022-05-26 16:00:49 +0300

    Adding the Header Exchange instance

---
+++ RabbitMq.Header.Publisher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using RabbitMq.Header.Publisher;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+Dictionary<string, object> headers = new Dictionary<string, object>();
+headers.Add("format", "pdf");
+headers.Add("shape2", "a4");
+
+var properties = channel.CreateBasicProperties();
+properties.Headers = headers;
+channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes("header mesajım"));
+Console.WriteLine($" [x] Log Sent");
---
+++ RabbitMq.Header.Subscriber/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+var queueName = channel.QueueDeclare().QueueName;
+
+Dictionary<string, object> headers = new Dictionary<string, object>();
+
+headers.Add("format", "pdf");
+headers.Add("shape", "a4");
+headers.Add("x-match", "any");
+
+channel.QueueBind(queueName, "header-exchange", String.Empty, headers);
+channel.BasicConsume(queueName, false, consumer);
+Console.WriteLine("Waiting for messages...");
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine(" [x] Received {0}", message);
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+
+Console.ReadLine();
