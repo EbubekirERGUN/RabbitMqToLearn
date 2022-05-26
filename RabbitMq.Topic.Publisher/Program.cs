27d79ee964ae13aef00ba5a91c4b27a3bc78273c
Ebubekir ERGÜN <ebubekir.ergun@procenne.com>
2022-05-26 15:51:26 +0300

    Adding a Topic Exchange instance

---
+++ RabbitMq.Topic.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using RabbitMq.Topic.Publisher;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
+
+Random rnd = new Random();
+
+Enumerable.Range(1, 50).ToList().ForEach(i =>
+{
+    LogNames log1 = (LogNames)rnd.Next(1, 5);
+    LogNames log2 = (LogNames)rnd.Next(1, 5);
+    LogNames log3 = (LogNames)rnd.Next(1, 5);
+
+    var routeKey = $"{log1}.{log2}.{log3}";
+    string message = $"log-type: {log1}-{log2}-{log3}";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "logs-topic",
+                         routingKey: routeKey,
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($" [x] Log Sent {message}");
+});
---
+++ RabbitMq.Topic.Subscriber/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+var QueueName = channel.QueueDeclare().QueueName;
+var routeKey = "*.Error.*";
+channel.QueueBind(QueueName, "logs-topic", routeKey);
+channel.BasicConsume(QueueName, false, consumer);
+Console.WriteLine("Waiting for messages...");
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine(" [x] Received {0}", message);
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+
+Console.ReadLine();
